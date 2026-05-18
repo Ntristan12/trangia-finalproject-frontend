853b69eada0f3747f45322d74732537deace8435
delcuyos <DelCuyos>
2026-05-19 07:15:05 +0800

    frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://cuyos-finalproject-backend.onrender.com'
+    apiUrl: 'https://trangia-finalproject-backend.onrender.com'
 };
