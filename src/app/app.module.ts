1013ccb422fc62c5e4e2f6f149100766de319cad
delcuyos <DelCuyos>
2026-05-18 08:47:59 +0800

    setting up app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { HomeComponent } from './home';
         { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        
 
         // provider used to create fake backend
         (environment.production ? [] : [fakeBackendProvider])
