0dd871940ec9e178985990923bd646e36ad82c09
Ntristan12 <tneiltristan@gmail.com>
2026-05-22 01:02:28 +0800

    fix: fixing environment prod ts typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const environment =   {
     production: true,
-    apiUrl: 'https://trangia-final-backend.onrender.com'
+    apiUrl: 'https://tranga-final-backend.onrender.com'
 };
